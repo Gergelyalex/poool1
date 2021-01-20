9c5dcdd051120c8b59aee28d344998f6088c72a5
Gergelyalex <72394958+Gergelyalex@users.noreply.github.com>
2021-01-20 23:55:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex15pool1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c= int.Parse(Console.ReadLine());
+            if(a>b && a>c && b>c)
+            {
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+
+                Console.WriteLine(c);
+
+
+            }
+        }
+    }
+}
